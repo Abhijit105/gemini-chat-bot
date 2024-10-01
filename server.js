a97c46070101af9df5822953631fd3a457449e31
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2024-10-01 22:30:30 +0530

    code

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
+const { chat } = require('./gemini-controller')
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const { GoogleGenerativeAI } = require('@google/generative-ai')
+
+dotenv.config()
 
 const app = express()
-dotenv.config()
 
 app.use(express.json())
 app.use(cors())
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY)
-
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, async () => {
   console.log(`Server listening on port ${process.env.PORT || 8080}`)
 })
 
+// app.post('/start', async (req, res) => {
+//   const reply = await startChat()
+//   return res.json({ status: 'success', message: reply })
+// })
+
 app.post('/generate', async (req, res) => {
-  const prompt = req.body.prompt
-  const response = await genAI.generateText(prompt)
-  res.send(response)
+  const { messageUser, history } = req.body
+  try {
+    const { status, messageError, messageGemini } = await chat(
+      messageUser,
+      history
+    )
+    if (status === 'success') {
+      return res.json({ status: 'success', messageGemini })
+    } else {
+      return res.status(500).json({ status: 'failure', messageError })
+    }
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ status: 'failure', messageError: error.message })
+  }
 })
--- src/App.js
+++ src/App.js
@@ -1,36 +1,122 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 const App = () => {
-  const [message, setMessage] = useState('')
+  const [messageUser, setMessageUser] = useState('')
+  const [history, setHistory] = useState([])
+  const [isGeminiResponsePaused, setIsGeminiResponsePaused] = useState(false)
+  const [isGeminiTooltipDisplayed, setIsGeminiTooltipDisplayed] =
+    useState(false)
+
+  const textareaRef = useRef()
+
+  const chatUser = () => {
+    setMessageUser('')
+    textareaRef.current.focus()
+  }
+
+  const getGeminiResponse = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          messageUser,
+          history,
+        }),
+      })
+      const data = await response.json()
+      const { status, messageGemini, messageError } = data
+      console.log(data)
+      if (status === 'failure') {
+        console.log(messageError)
+        return
+      } else if (status === 'success') {
+        const chatGemini = {
+          role: 'model',
+          parts: [{ text: messageGemini }],
+        }
+        !!messageGemini && setHistory(history => [...history, chatGemini])
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setMessageUser('')
+      textareaRef.current.focus()
+    }
+  }
+
+  const sendMessage = async e => {
+    e.preventDefault()
+    if (!messageUser) return
+    const chatUser = {
+      role: 'user',
+      parts: [{ text: messageUser }],
+    }
+    !!messageUser && setHistory(history => [...history, chatUser])
+    if (isGeminiResponsePaused) return
+    await getGeminiResponse()
+  }
+
+  const chatGemini = async () => {
+    if (isGeminiResponsePaused) return
+    await getGeminiResponse()
+  }
 
   return (
     <main className='main'>
-      <h1 className='title'>Hello Gemini</h1>
+      <h1 className='title'>Hello Gemini 🤖</h1>
       <div className='buttons'>
-        <button className='btn'>Go ahead and start chat</button>
-        <button className='btn'>Let Gemini take the lead</button>
+        <button className='btn' onClick={chatUser}>
+          {history && history.length === 0
+            ? 'Go ahead and start chat'
+            : 'Keep on talking'}
+        </button>
+        <div className='btn-container'>
+          <button
+            className='btn'
+            onClick={chatGemini}
+            onMouseOver={() => setIsGeminiTooltipDisplayed(true)}
+            onMouseOut={() => setIsGeminiTooltipDisplayed(false)}
+            onContextMenu={() =>
+              setIsGeminiResponsePaused(!isGeminiResponsePaused)
+            }
+          >
+            {!!history && history.length === 0
+              ? 'Let Gemini take the lead'
+              : 'Now Gemini talks'}
+          </button>
+          {isGeminiTooltipDisplayed && (
+            <div className='tooltip'>
+              Right click to {isGeminiResponsePaused ? 'unpause' : 'pause'}{' '}
+              Gemini
+            </div>
+          )}
+        </div>
       </div>
-      <form className='form'>
+      <form className='form' onSubmit={e => sendMessage(e)}>
         <textarea
-          value={message}
-          onChange={e => setMessage(e.target.value)}
+          value={messageUser}
+          onChange={e => setMessageUser(e.target.value)}
           className='textarea'
+          ref={textareaRef}
         ></textarea>
         <button type='submit' className='btn send'>
           Send
         </button>
       </form>
-      <div className='chat'>
-        <div className='chat-box'>
-          <div className='chat-user'>
-            <p className='label-user'>User:</p>
-            <p className='text-user'>Hi! How are you?</p>
-          </div>
-          <div className='chat-gemini'>
-            <p className='label-gemini'>Gemini:</p>
-            <p className='text-gemini'>Hi! I'm fine!</p>
-          </div>
-        </div>
+      <div className='chat-box'>
+        {!!history &&
+          history.length !== 0 &&
+          history.map((chat, index) => (
+            <div className='chat' key={index}>
+              <p className='chat-label'>
+                {chat.role === 'user' ? 'User:' : 'Gemini:'}
+              </p>
+              <p className='chat-text'>{chat.parts[0].text}</p>
+            </div>
+          ))}
       </div>
     </main>
   )
